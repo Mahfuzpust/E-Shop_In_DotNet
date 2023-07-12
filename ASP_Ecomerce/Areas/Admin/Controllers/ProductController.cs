310d075461114968d6952eec7cc2dbf6e341d077
mahfuz khan <mahfuzpust@gmail.com>
2023-07-13 00:40:10 +0600

    Product-Image-Edit

--- ASP_Ecomerce/Areas/Admin/Controllers/ProductController.cs
+++ ASP_Ecomerce/Areas/Admin/Controllers/ProductController.cs
@@ -42,7 +42,56 @@ namespace ASP_Ecomerce.Areas.Admin.Controllers
                     products.Image = "Images/" + image.FileName;
                 }
 
+                if(image == null)
+                {
+                    products.Image = "Images/NoIma.jpg";
+                }
+
                 dbContext.Products.Add(products);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        //Edit Get Product
+        public IActionResult Edit(int id)
+        {
+            ViewData["productTypeId"] = new SelectList(dbContext.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(dbContext.SpecialTags.ToList(), "Id", "SpecialTag");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = dbContext.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTags)
+                .FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //POST Edit Method
+        [HttpPost]
+        public async Task<IActionResult> Edit(Products products, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    var name = Path.Combine(env.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    products.Image = "Images/" + image.FileName;
+                }
+
+                if (image == null)
+                {
+                    products.Image = "Images/NoIma.jpg";
+                }
+
+                dbContext.Products.Update(products);
                 await dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
