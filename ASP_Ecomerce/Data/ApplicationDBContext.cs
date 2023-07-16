01b1071c04232d0571c3484fcaba10676c23d632
mahfuz khan <mahfuzpust@gmail.com>
2023-07-16 15:47:26 +0600

    Order-OrderDetails-Models

--- ASP_Ecomerce/Data/ApplicationDBContext.cs
+++ ASP_Ecomerce/Data/ApplicationDBContext.cs
@@ -16,5 +16,7 @@ namespace ASP_Ecomerce.Data
         public DbSet<ProductTypes> ProductTypes { get; set; }
         public DbSet<SpecialTags> SpecialTags { get; set; }
         public DbSet<Products> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
---
+++ ASP_Ecomerce/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Ecomerce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Display(Name = "Order No")]
+        public string OrderNo { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Phone No")]
+        public int PhoneNo { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public virtual List<OrderDetails> OrderDetails { get; set; }
+    }
+}
